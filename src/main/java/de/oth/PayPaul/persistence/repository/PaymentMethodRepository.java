fe15bfe37b709caefdc301c129fea35708181e02
paul.pongratz94@gmail.com <paul.pongratz94@gmail.com>
2019-12-30 01:05:18 +0100

    Bugfix paymentmethod repo

--- src/main/java/de/oth/PayPaul/persistence/repository/PaymentMethodRepository.java
+++ src/main/java/de/oth/PayPaul/persistence/repository/PaymentMethodRepository.java
@@ -10,10 +10,10 @@ import org.springframework.data.repository.query.Param;
 import java.util.List;
 
 public interface PaymentMethodRepository extends CrudRepository<PaymentMethod, Integer> {
-  @Query("SELECT M FROM Account A LEFT JOIN A.paymentMethods P INNER JOIN CreditCard M ON P.id = M.id WHERE A.email=:email")
+  @Query("SELECT M FROM Account A LEFT JOIN A.paymentMethods P INNER JOIN CreditCard M ON P.id = M.id WHERE A.email=:email and M.cardNumber IS NOT null")
   public List<CreditCard> findAllCreditCardsForUser(@Param("email") String email);
 
-  @Query("SELECT M FROM Account A LEFT JOIN A.paymentMethods P INNER JOIN BankAccount M ON P.id = M.id WHERE A.email=:email")
+  @Query("SELECT M FROM Account A LEFT JOIN A.paymentMethods P INNER JOIN BankAccount M ON P.id = M.id WHERE A.email=:email and M.IBAN IS NOT null")
   public List<BankAccount> findAllBankAccountsForUser(@Param("email") String email);
 
   @Query("SELECT CASE WHEN COUNT(M) > 0 THEN TRUE ELSE FALSE END FROM Account A LEFT JOIN A.paymentMethods P INNER JOIN BankAccount M ON P.id = M.id where M.IBAN=:iban and A.email=:email")
